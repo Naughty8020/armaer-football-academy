412036ecdc5d53b5a107a52eeea3ed1bf7e70643
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-08-19 02:18:35 +0900

    del-config-img-microcms

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  images: {
-    domains: ["armaer-football.microcms.io"],
-  },
+  
 };
 
 export default nextConfig;
