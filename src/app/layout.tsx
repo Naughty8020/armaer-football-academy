7712d00a42f12682f32aae6b5fd55b46dbb08915
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-09-27 14:08:30 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     url: 'https://armaerfootball.com',
     images: [
       {
-        url: 'https://armaerfootball.com/public/og-image.jpg',
+        url:'https://armaerfootball.com/ogb.png',
         width: 1200,
         height: 630,
         alt: 'Armaer Football Academy - サッカースクールのイメージ',
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Armaer Football Academy | 滋賀県近江八幡市で活動しているサッカースクール',
     description: '世界基準のサッカー選手育成プログラムを提供するサッカースクール。',
-    images: ['https://armaerfootball.com/images/og-image.jpg'],
+    images: ['https://armaerfootball.com/ogb.png'],
   },
   icons: {
     icon: '/favicon.ico',
@@ -44,33 +44,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Headerpage />
         <main className="bg-white">{children}</main>
         <Footer />
-     
         <Head>
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify({
-                '@context': 'http://schema.org',
-                '@type': 'Organization',
-                name: 'Armaer Football Academy',
-                url: 'https://armaerfootball.com',
-                logo: 'https://armaerfootball.com/images/logo.png',
-                contactPoint: {
-                  '@type': 'ContactPoint',
-                  telephone: '+81-123-456-7890',
-                  contactType: 'Customer Service',
-                  areaServed: 'JP',
-                  availableLanguage: 'Japanese',
-                },
-                sameAs: [
-                  'https://www.facebook.com/armaerfootball',
-                  'https://twitter.com/armaerfootball',
-                  'https://www.instagram.com/armaerfootball',
-                ],
-              }),
-            }}
-          />
-        </Head>
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify({
+        '@context': 'http://schema.org',
+        '@type': 'Organization',
+        name: 'Armaer Football Academy',
+        url: 'https://armaerfootball.com',
+        logo: 'https://armaerfootball.com/ogb.png', // ← ここ
+        contactPoint: {
+          '@type': 'ContactPoint',
+          telephone: '+81-123-456-7890',
+          contactType: 'Customer Service',
+          areaServed: 'JP',
+          availableLanguage: 'Japanese',
+        },
+        sameAs: [
+          'https://www.facebook.com/armaerfootball',
+          'https://twitter.com/armaerfootball',
+          'https://www.instagram.com/armaerfootball',
+        ],
+      }),
+    }}
+  />
+</Head>
+
       </body>
     </html>
   );
